dce705888c78956f00fe8958c6b7a70fc8b4803b
sky0621 <sky14work@gmail.com>
2017-09-01 01:47:37 +0900

    ch01 continue

---
+++ ch01/main1_5_3.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+print(x + y)
+
+print(x - y)
+
+print(x * y)
+
+print(x / y)
+
+xx = np.array([[1, 2], [3, 4]])
+print(xx)
+
+print(xx.shape)
+
+print(xx.dtype)
